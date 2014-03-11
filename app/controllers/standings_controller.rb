29c1a1985f8284cdf466ace889f0a279a693638c
Vince-J <vincent.jaouen@gmail.com>
2014-03-11 21:42:50 +0100

    refactor code from controller to model

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -10,62 +10,64 @@ class StandingsController < ApplicationController
   # PATCH/PUT /stages/1/groups/1/standings
   def update
     @standings.each do |standing|
-      
-      standing.played = 0
-      standing.won = 0
-      standing.draw = 0
-      standing.lost = 0
-      standing.goals_against = 0
-      standing.goals_for = 0
-      standing.points = 0
 
-      matches = Match.where("(team_a_id = ? or team_b_id = ?) and results.played = 't'", standing.team_id, standing.team_id)
+      standing.compute_and_update
 
-      matches.each do |match|
 
-        standing.played += 1
+      # standing.played = 0
+      # standing.won = 0
+      # standing.draw = 0
+      # standing.lost = 0
+      # standing.goals_against = 0
+      # standing.goals_for = 0
+      # standing.points = 0
 
-        # team_a
-        if match.team_a_id == standing.team_id
-          standing.goals_for += match.result.score_team_a
-          standing.goals_against += match.result.score_team_b
-          if match.result.score_team_a > match.result.score_team_b
-            standing.won += 1
-            standing.points += 3
-          elsif match.result.score_team_a == match.result.score_team_b
-            standing.draw += 1
-            standing.points += 1
-          else
-            standing.lost += 1
-          end
-        # team_b
-        else
-          standing.goals_for += match.result.score_team_b
-          standing.goals_against += match.result.score_team_a
-          if match.result.score_team_a > match.result.score_team_b
-            standing.lost += 1
-          elsif match.result.score_team_a == match.result.score_team_b
-            standing.draw += 1
-            standing.points += 1
-          else
-            standing.won += 1
-            standing.points += 3
-          end
-        end
+      # matches = Match.where("(team_a_id = ? or team_b_id = ?) and results.played = 't'", standing.team_id, standing.team_id)
 
-      end
+      # matches.each do |match|
 
-      if standing.save
-        logger.debug "standing saved #{standing.inspect}"
-      else
-        logger.error "error saving standing #{standing.inspect}"
-      end
+      #   standing.played += 1
+
+      #   # team_a
+      #   if match.team_a_id == standing.team_id
+      #     standing.goals_for += match.result.score_team_a
+      #     standing.goals_against += match.result.score_team_b
+      #     if match.result.score_team_a > match.result.score_team_b
+      #       standing.won += 1
+      #       standing.points += 3
+      #     elsif match.result.score_team_a == match.result.score_team_b
+      #       standing.draw += 1
+      #       standing.points += 1
+      #     else
+      #       standing.lost += 1
+      #     end
+      #   # team_b
+      #   else
+      #     standing.goals_for += match.result.score_team_b
+      #     standing.goals_against += match.result.score_team_a
+      #     if match.result.score_team_a > match.result.score_team_b
+      #       standing.lost += 1
+      #     elsif match.result.score_team_a == match.result.score_team_b
+      #       standing.draw += 1
+      #       standing.points += 1
+      #     else
+      #       standing.won += 1
+      #       standing.points += 3
+      #     end
+      #   end
+
+      # end
+
+      # if standing.save
+      #   logger.debug "standing saved #{standing.inspect}"
+      # else
+      #   logger.error "error saving standing #{standing.inspect}"
+      # end
 
     end
 
-    respond_to do |format|
-      format.html { redirect_to stage_group_standings_path(params[:stage_id], params[:group_id]), notice: 'Standings were successfully updated.'}
-    end
+    redirect_to stage_group_standings_path(params[:stage_id], params[:group_id])
+
   end
 
   private
--- app/models/standing.rb
+++ app/models/standing.rb
@@ -11,4 +11,58 @@ class Standing < ActiveRecord::Base
 
 	# validation
 	validates :played, :won, :draw, :lost, :goals_for, :goals_against, :points, numericality: { greater_than_or_equal_to: 0 }
+
+	# compute standings
+	def compute_and_update
+
+    self.played = 0
+    self.won = 0
+    self.draw = 0
+    self.lost = 0
+    self.goals_against = 0
+    self.goals_for = 0
+    self.points = 0
+
+    matches = Match.where("(team_a_id = ? or team_b_id = ?) and results.played = 't'", self.team_id, self.team_id)
+
+    matches.each do |match|
+
+      self.played += 1
+
+      # team_a
+      if match.team_a_id == self.team_id
+        self.goals_for += match.result.score_team_a
+        self.goals_against += match.result.score_team_b
+        if match.result.score_team_a > match.result.score_team_b
+          self.won += 1
+          self.points += 3
+        elsif match.result.score_team_a == match.result.score_team_b
+          self.draw += 1
+          self.points += 1
+        else
+          self.lost += 1
+        end
+      # team_b
+      else
+        self.goals_for += match.result.score_team_b
+        self.goals_against += match.result.score_team_a
+        if match.result.score_team_a > match.result.score_team_b
+          self.lost += 1
+        elsif match.result.score_team_a == match.result.score_team_b
+          self.draw += 1
+          self.points += 1
+        else
+          self.won += 1
+          self.points += 3
+        end
+      end
+    end
+
+    if self.save
+      logger.debug "standing saved #{self.inspect}"
+    else
+      logger.error "error saving standing #{self.inspect}"
+    end
+	end
+
 end
